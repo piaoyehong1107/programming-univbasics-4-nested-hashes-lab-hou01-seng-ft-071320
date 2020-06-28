3de9fbefa71825385ac0fd5c1a8524e46beb58ff
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-28 05:38:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:known_for]="GUI"
+programmer_hash[:dennis_ritchie][:languages].push()
 programmer_hash
 end
